63a0c3e6f225d8bd9a03d729313ad1e480886ab5
Loqi <ProFoundNine856@gmail.com>
2025-09-22 12:03:21 +0300

    неработающий скрипт

---
+++ scripts.js
@@ -0,0 +1,10 @@
+<script>
+document.getElementById("address").addEventListener("change", function() {
+    let result = document.getElementById("result");
+    if (this.value) {
+        result.textContent = "Вы выбрали адрес: " + this.options[this.selectedIndex].text;
+    } else {
+        result.textContent = "";
+    }
+});
+</script>
